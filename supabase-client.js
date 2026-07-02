bcd989ce7350e444d4ce599c285ab5587eecc874
Thibaut C. <onirim.fox@proton.me>
2026-07-02 18:53:57 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
 // ── Config Supabase partagée ──────────────────────────────────
-const SUPABASE_URL = 'https://hknelrnwdcoqdgodtuwc.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_0NulxPP52dIAUMXcYoHrcA_z7NAfeW5';
+const SUPABASE_URL = 'https://fzmkoivplbvevvewcjul.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_7f5RyzpIj5dN_D-yg9fWXg_a3qetO4i';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
